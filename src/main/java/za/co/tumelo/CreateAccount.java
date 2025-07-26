5ce76dd881486c0e54186aaf3c78d815fcdab4e2
Tumelo <tumelontsilo@gmail.com>
2025-07-26 18:22:26 +0200

    Updated the createAccount class

--- src/main/java/za/co/tumelo/CreateAccount.java
+++ src/main/java/za/co/tumelo/CreateAccount.java
@@ -4,7 +4,7 @@ import org.json.JSONObject;
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.Date;
+import java.util.Random;
 import java.util.Scanner;
 
 public class CreateAccount {
@@ -17,35 +17,64 @@ public class CreateAccount {
     }
 
     public void enterDetails(){
-        System.out.println("Please Enter your name: ");
-        String name = sc.nextLine();
-        System.out.println("Please Enter your surname: ");
-        String surname = sc.nextLine();
-        System.out.println("Enter your year of birth: ");
-        int yearOfBirth = sc.nextInt();
-        System.out.println("Enter your month of birth(e.g February put 2: ");
-        int monthOfBirth = sc.nextInt();
-        System.out.println("Enter your day of birth (e.g 7): ");
-        int dayOfBirth = sc.nextInt();
+        while(true){
+            try{
+                System.out.println("Please Enter your name: ");
+                String name = sc.nextLine().trim();
+                if(name.isEmpty()) throw new IllegalArgumentException();
 
-        LocalDate dateOfBirth;
-        try{
-            dateOfBirth = LocalDate.of(yearOfBirth,monthOfBirth, dayOfBirth);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+                System.out.println("Please Enter your surname: ");
+                String surname = sc.nextLine().trim();
+                if(surname.isEmpty()) throw new IllegalArgumentException();
+
+                System.out.println("Enter your year of birth: ");
+                int yearOfBirth = sc.nextInt();
+
+                System.out.println("Enter your month of birth(e.g February put 2: ");
+                int monthOfBirth = sc.nextInt();
+
+                System.out.println("Enter your day of birth (e.g 7): ");
+                int dayOfBirth = sc.nextInt();
+
+                LocalDate dateOfBirth;
+                try{
+                    dateOfBirth = LocalDate.of(yearOfBirth,monthOfBirth, dayOfBirth);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+
+                int age = Period.between(dateOfBirth, LocalDate.now()).getYears();
+                if(age < 18){
+                    System.out.println("Sorry " + name + " you are too young to open any account with us!");
+                    System.out.println("Exiting, please enjoy the rest of your day.");
+                    System.exit(0);
+                }
+
+                personalDetails.put("Name", name);
+                personalDetails.put("Surname", surname);
+                personalDetails.put("Date of birth", dateOfBirth.toString());
+                personalDetails.put("Age", age);
+                break;
+            } catch (RuntimeException e) {
+                System.out.println("Invalid inputs");
+                sc.nextLine();
+                System.out.println("Let us try again!");
+            }
+
         }
-
-        int age = Period.between(dateOfBirth, LocalDate.now()).getYears();
-
-
-        personalDetails.put("Name", name);
-        personalDetails.put("Surname", surname);
-        personalDetails.put("Date of birth", dateOfBirth.toString());
-        personalDetails.put("Age", age);
 
     }
 
     public JSONObject getPersonalDetails() {
         return personalDetails;
+    }
+
+    public void printPersonalDetails(){
+        System.out.println(this.personalDetails);
+    }
+
+    public int createPin(){
+        Random rd = new Random();
+        return rd.nextInt(9000) + 1000;
     }
 }
--- src/main/java/za/co/tumelo/Main.java
+++ src/main/java/za/co/tumelo/Main.java
@@ -6,18 +6,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to TEN-Bank...");
-        System.out.println("Do you have an account with us? (yes/no)");
-        String answer = sc.nextLine();
+        System.out.println("Now let us open an account for you.");
 
-        if(answer.equalsIgnoreCase("yes")){
-            System.out.println("Okay, please enter you 4 digit pin: ");
-            int pin = sc.nextInt();
-        }else if(answer.equalsIgnoreCase("no")){
-            System.out.println("Let's open one for you then.");
-            CreateAccount createAccount = new CreateAccount();
-            createAccount.enterDetails();
+        CreateAccount createAccount = new CreateAccount();
+        createAccount.enterDetails();
+        createAccount.printPersonalDetails();
 
-        }
+        System.out.println("Here is your pin number, use it login");
+        System.out.println("Pin: " + createAccount.createPin());
 
     }
 }
