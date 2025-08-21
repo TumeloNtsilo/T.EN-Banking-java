96620ed84ff1e51578ca316c38ba457b96769339
Tumelo <tumelontsilo@gmail.com>
2025-08-21 17:52:11 +0200

    Updated the ClientHandler

--- src/main/java/za/co/tumelo/server/ClientHandler.java
+++ src/main/java/za/co/tumelo/server/ClientHandler.java
@@ -1,12 +1,20 @@
 package za.co.tumelo.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ClientHandler implements Runnable{
     private final Socket clientSocket;
+    private BufferedReader in;
+    private PrintWriter out;
 
-    public ClientHandler(Socket clientSocket){
+    public ClientHandler(Socket clientSocket) throws IOException {
         this.clientSocket = clientSocket;
+        this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        this.out = new PrintWriter(clientSocket.getOutputStream(), true);
     }
 
 
