472f7aab939457a4cc15b84fdb9925bc1414d26b
Tumelo <tumelontsilo@gmail.com>
2025-08-21 18:21:45 +0200

    Created the client class

--- src/main/java/za/co/tumelo/client/Client.java
+++ src/main/java/za/co/tumelo/client/Client.java
@@ -1,5 +1,23 @@
 package za.co.tumelo.client;
 
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client {
+    private static final int port = 6000;
+    private static final String host = "localhost";
+
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket(host, port);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream());
+    }
+
+
 
 }
