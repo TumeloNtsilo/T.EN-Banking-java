cfc0ec17ad8ab800670cb9529ee800e9429cd2e2
Tumelo <tumelontsilo@gmail.com>
2025-09-11 22:35:30 +0200

    Updated the script file

--- script/run_client.sh
+++ script/run_client.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-echo "compiling..."
+echo "compiling and starting client..."
+
 cd /home/tumelo_ntsilo/Github_work/T.EN-Banking-java
 mvn compile exec:java -Dexec.mainClass="za.co.tumelo.client.Client"
-
-
-#Start client
-echo
-echo "Starting the client"
-java -cp target/classes za/co/tumelo/client/Client
 
 sleep 2
